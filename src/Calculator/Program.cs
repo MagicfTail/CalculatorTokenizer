3370af868ee8b06bc0f0bd3fbe2612dc053b453f
Magic fTail <runecnielsen3132@gmail.com>
2023-10-17 19:18:35 +0200

    Add the entire thing

---
+++ src/Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Tokenizer;
+
+
+Tokenizer.Tokenizer tokenizer = new();
+
+while (true)
+{
+    Console.WriteLine("-----Enter Calculation-----");
+    string? input = Console.ReadLine();
+
+    if (input == null || input == "")
+    {
+        continue;
+    }
+
+    tokenizer.Input = input;
+    try
+    {
+        var output = tokenizer.Tokenize();
+
+        Console.WriteLine($"Result: {output.Eval()}");
+    }
+    catch (InvalidDataException e)
+    {
+        Console.WriteLine($"Invalid input: {e.Message}");
+    }
+}
